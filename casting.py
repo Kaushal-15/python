bf99c3a82f701483b04152c3ad2366974f3d3b8a
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-10-06 20:19:55 +0530

    file added

---
+++ casting.py
@@ -0,0 +1,5 @@
+#it is casting which converts one datatype to another if int is removed in both the a and b the result will be 1020,ater addind casting it gives the coreect output as 30.
+a=int("10")
+b=int("20")
+c=a+b
+print(c)
