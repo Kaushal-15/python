59f89fc14a53cc495ca8f61ac2e1fb1f8c429ac1
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-10-07 12:26:31 +0530

    file

---
+++ elif.py
@@ -0,0 +1,14 @@
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+operand=input("add/sub/multi/div:")
+if(operand=='add'):
+    print(a+b)
+elif(operand=='sub'):
+    print(a-b)
+elif(operand=='multi'):
+    print(a*b)
+elif(operand=='div'):
+    print(a/b)
+else:
+        print('invalid')
+#calculator
