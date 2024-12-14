e9c9a2c5493d382c778fafc1c5c97ac8000949c0
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-14 12:52:27 +0530

    file added

--- random.py
+++ random.py
@@ -3,7 +3,7 @@ def get_choices():
     player_choice = input("Enter the choice(rock,paper,scissors)")
     options = ["rock","paper","scissors"]
     computer_choice = random.choice(options)
-    choices = {"player":player_choice,"computer":computer_choice}
+    choices = {"player":player_choice,"computer":computer_choice} 
     return choices
 
 
