bd9d785f648ce9a61902611c1f818534dfc1bd0b
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-16 12:41:24 +0530

    file added

---
+++ op.py
@@ -0,0 +1,7 @@
+#print the number of integers that divide by both 3 and 5 using logical operator
+count=0
+for i in range(1,100):
+    if(i%3 == 0 and i%5 == 0):
+        count=count+1
+print(count)
+
