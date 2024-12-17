e73bd643af05e82e80a1cd4af3da16cb20baef6a
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-17 17:13:00 +0530

    file added

---
+++ class_obj.py
@@ -0,0 +1,19 @@
+#class and objects 
+class goa:
+    name=""
+    drink=""
+    def party(self):
+        print("Lets enjoy")
+    def beach(self):
+        print("Beach")
+
+ramesh = goa()
+suresh = goa()
+ramesh.name="Ramesh"
+ramesh.drink="Watermelon"
+suresh.name="suresh"
+suresh.drink="lemonade"
+print(ramesh.name)
+print(ramesh.drink)
+print(suresh.name)
+print(suresh.drink)
