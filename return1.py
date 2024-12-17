bfa50e44934adb3a5e4776fa2e4e6ac070e47b1e
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-17 12:58:01 +0530

    file added

---
+++ return1.py
@@ -0,0 +1,12 @@
+#return ex
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+c=int(input("Enter c:"))
+def add(c,d):
+    return c+d
+
+
+added=add(a,b)
+multiply=added*c
+print(multiply)
+add(a,b)
