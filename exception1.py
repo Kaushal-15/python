0505e8b0c03251dca244ab322569de9729ed9dc9
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-19 11:33:30 +0530

    file added

---
+++ exception1.py
@@ -0,0 +1,8 @@
+#if we use the keyword Exception then every error will be handled if we specify the error then it will only be catched 
+try:
+    a=int(input())
+    b=int(input())
+    c=input()#here i would give input as 20 or any other integer then it will be type error then run time error will occur as the exception is only for valueError
+    print(c/a)
+except ValueError as e:
+    print(e)
