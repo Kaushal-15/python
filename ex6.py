e9f1209b377c8dba0bd5348321552fec745dd469
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-24 22:18:11 +0530

    file added

---
+++ ex6.py
@@ -0,0 +1,10 @@
+print("****************************")
+print("Welcome to login page:")
+user="kaushal"
+password=12345
+username=input("Enter the username:")
+Password=int(input("Enter the password")) 
+if(user==username and password==Password):
+    print(f"Welcome {username} to the Webpage.")
+else:
+    print("Invalid Credentails")
