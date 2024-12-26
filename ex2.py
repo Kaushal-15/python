88edf7dc00fec6d12aa8b023ba5232140b3e4a44
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2024-12-26 15:24:55 +0530

    File added

---
+++ ex2.py
@@ -0,0 +1,15 @@
+user="kaushal"
+
+password=12345
+
+User-input("Enter the username:")
+
+Password=int(input("Enter the Password:"))
+
+if (user==User and password==Password):
+
+print (f"Welcome to the login page (user).")
+
+else:
+
+print("Invalid Credentials")
