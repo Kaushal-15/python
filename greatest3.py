ca17f40365c59455df289f1f89cb83049c6ef2da
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-08 10:16:00 +0530

    file added

---
+++ greatest3.py
@@ -0,0 +1,10 @@
+if (a >= c):
+    if (a > b):
+        print(f"{a} is the greatest number among {a}, {b}, {c}")
+    else:
+        print(f"{b} is the greatest number among {a}, {b}, {c}")
+else:
+    if (b > c):
+        print(f"{b} is the greatest number among {a}, {b}, {c}")
+    else:
+        print(f"{c} is the greatest number among {a}, {b}, {c}")
