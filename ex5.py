ede807df783e6cf12a709d196b02f36086434806
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-01-29 21:42:07 +0530

    Update

---
+++ ex5.py
@@ -0,0 +1,32 @@
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade
+
+    def get_name(self):
+        return self.name
+
+    def get_age(self):
+        return self.age
+
+    def get_grade(self):
+        return self.grade
+
+    def set_name(self, name):
+        self.name = name
+
+    def set_age(self, age):
+        self.age = age
+
+    def set_grade(self, grade):
+        self.grade = grade
+
+    def display(self):
+        print(f"{self.name}")
+        print(f"{self.age}")
+        print(f"{self.grade}")
+
+
+student = Student("Akash", 18, "A")
+print(student.get_name())
