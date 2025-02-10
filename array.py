8566db6eb8fea9beed508e4934da4572e004e343
Kaushal-15 <153284664+Kaushal-15@users.noreply.github.com>
2025-02-10 17:59:53 +0530

    Create array.py

---
+++ array.py
@@ -0,0 +1,9 @@
+import numpy as np
+a=np.array([1,24,45])
+b=np.array([[1,2,3,4,],[22,44,53,51]])
+#to check the dimension of the array
+print(b.shape)
+print(a.shape)
+#to check the size that is the total elements of the array
+print(a.size)
+print(b.size)
